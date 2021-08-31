62439f62b7c42ace86f6ecc0230cc5eb75bb815d
Paul Niezborala <p.niezborala@wewanttoknow.com>
2021-08-31 23:42:20 +0200

    [Feature] Ruboku::Grid: implement from_grid to pass grid directly

--- lib/ruboku/grid.rb
+++ lib/ruboku/grid.rb
@@ -1,5 +1,33 @@
 module Ruboku
   class Grid
+    class << self
+      def from_grid(array_of_arrays)
+        raise ArgumentError, 'Not a grid' unless
+          array_of_arrays.is_a?(Array) and
+          array_of_arrays.all? { |array| array.is_a? Array }
+        raise ArgumentError, 'Not a standard layout' unless
+          is_perfect_square?(array_of_arrays.count) and
+          array_of_arrays.all? { |array| array.count == array_of_arrays.count }
+
+        size = Math.sqrt(array_of_arrays.count).to_i
+        max_value = size * size
+        acceptable_values = (1..max_value).to_a
+
+        raise ArgumentError, 'Unexpected values' if array_of_arrays.flatten.any? { |value| !acceptable_values.include?(value) }
+
+        grid = self.new(size, fill: false)
+        grid.grid = array_of_arrays
+        return grid
+      end
+
+      private
+
+      def is_perfect_square?(value)
+        square_root_i = Math.sqrt(value).to_i
+        square_root_i * square_root_i == value.to_i
+      end
+    end
+
     attr_accessor :grid, :size, :max_value, :acceptable_values
     include Enumerable
 
